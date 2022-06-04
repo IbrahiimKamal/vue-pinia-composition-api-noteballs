c5dd59b66b5b8d11a7fd992bc49b48bedb3e199c
ibrahiimKamal <ibrahim.kamal@starsandstories.com>
2022-06-04 19:14:57 +0200

    add notes delete notes functionality

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,32 +3,69 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add New Note" />
+          <textarea
+            v-model="newNote"
+            ref="newNoteRef"
+            class="textarea"
+            placeholder="Add New Note"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
             Add New Note
           </button>
         </div>
       </div>
     </div>
 
-    <div v-for="i in 3" :key="i" class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste iure
-          doloribus cupiditate ipsum nostrum, eligendi inventore incidunt
-          aliquam consequatur accusamus quis et deserunt praesentium expedita
-          eaque modi fugit repudiandae eius.
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <Note
+      v-for="note in notes"
+      :key="note.id"
+      :note="note"
+      @deleteNote="deleteNote"
+    />
   </div>
 </template>
+
+<script setup>
+/* ######## imports ######## */
+import { ref } from "vue";
+import Note from "@/components/Notes/Note.vue";
+
+/* ######## notes ######## */
+const newNote = ref("");
+const newNoteRef = ref(null);
+
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste iure doloribus cupiditate ipsum nostrum, eligendi inventore incidunt aliquam consequatur accusamus quis et deserunt praesentium expedita eaque modi fugit repudiandae eius.",
+  },
+  {
+    id: "id2",
+    content: "this is a short note",
+  },
+]);
+
+// methods
+const addNote = () => {
+  notes.value.unshift({
+    id: new Date().getTime().toString(),
+    content: newNote.value,
+  });
+  newNote.value = "";
+  newNoteRef.value.focus();
+};
+
+const deleteNote = (id) => {
+  notes.value = notes.value.filter((note) => note.id !== id);
+};
+</script>
